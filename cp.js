09f03ba7672b2be270581a82d4f22a4cff5b8138
suraj_dell_linux <surazmah@gmail.com>
2023-05-14 10:19:01 -0700

    adding changes

--- cp.js
+++ cp.js
@@ -1,9 +1,17 @@
-function copyCode(codeid) {
+const copyButtonLabel = "Copy Code";
+
+async function copyCodetest(button, codeid) {
     var copyText = document.getElementById(codeid);
   
      // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.textContent);
+    await navigator.clipboard.writeText(copyText.textContent);
+  
+    // visual feedback that task is completed
+    button.innerText = "Code Copied";
 
-    // Alert the copied text
-    // alert("Copied the text: " + copyText.textContent);
+    setTimeout(() => {
+      button.innerText = copyButtonLabel;
+    }, 700);
   }
+
+// https://www.roboleary.net/2022/01/13/copy-code-to-clipboard-blog.html#javascript

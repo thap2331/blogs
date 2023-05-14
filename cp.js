7f9247e2cd42879c9e6ce4770ea04ab3f7545add
suraj_dell_linux <surazmah@gmail.com>
2023-05-14 16:59:13 -0700

    more changes on color

--- cp.js
+++ cp.js
@@ -10,7 +10,7 @@ async function copyCodetest(button, codeid) {
     setTimeout(() => {
       button.innerText = "Copy Code";
     }, 700);
-  }
+  };
 
 // https://www.roboleary.net/2022/01/13/copy-code-to-clipboard-blog.html#javascript
 
@@ -19,4 +19,18 @@ function expandTips(section){
 
   tipsList.style.display = tipsList.style.display == 'block' ? 'none' : 'block';
   section.getElementsByTagName('p')[0].innerText = tipsList.style.display == 'block' ? '-' : '+';
+};
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
